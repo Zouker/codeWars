7689103e239630d59574f8c1d6b5c76360f7ca2d
Denis Shubin <denis.shubin@gmail.com>
2022-02-18 19:18:51 +0300

    fix

--- 01.js
+++
@@ -1,7 +0,0 @@
-function solution(str){
-    const array = str.split('');
-    const reversedStr = array.reduce((reversed, character) => {
-      return character + reversed
-    }, '')
-    return reversedStr
-}
