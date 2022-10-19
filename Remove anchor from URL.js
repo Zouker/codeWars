bc9475546e5bff33566838f57413e5477fd18b71
Denis Shubin <denis.shubin@gmail.com>
2022-10-19 11:47:01 +0300

    Remove anchor from URL

---
+++ Remove anchor from URL.js
@@ -0,0 +1,9 @@
+// Remove anchor from URL
+//
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+//
+//     Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+const removeUrlAnchor = (url) => url.indexOf('#') === -1 ? url : url.slice(0, url.indexOf('#'))
